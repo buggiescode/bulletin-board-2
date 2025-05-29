6af74aa469b79d16584b72d83dbc539d990e59b6
buggiescode <137247769+buggiescode@users.noreply.github.com>
2025-05-29 05:13:28 +0000

    finished sign up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  # For Devise User Password stuff
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
   # Allow server to be hosted on any URL
   config.hosts.clear
   # Allow better_errors to work in online IDE
@@ -34,7 +37,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
@@ -85,7 +88,4 @@ Rails.application.configure do
 
   # Allow POST authenticity on Codespaces in dev
   config.action_controller.forgery_protection_origin_check = false
-
-  # For Devise User Password stuff
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
